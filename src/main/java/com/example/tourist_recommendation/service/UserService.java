6e8e9f2e7c2cf535e038db0ca613b628751910d9
qncj5224 <qncj5224@naver.com>
2024-12-27 22:20:48 +0900

    주석 보완: UserService 클래스 및 메서드 설명 추가

--- src/main/java/com/example/tourist_recommendation/service/UserService.java
+++ src/main/java/com/example/tourist_recommendation/service/UserService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 /**
  * 사용자 관련 비즈니스 로직을 처리하는 서비스 클래스.
- * 사용자 저장, 조회, 중복 체크, 회원가입 등 기능을 제공한다.
+ * 사용자 저장, 조회, 중복 체크, 회원가입 등 기능을 제공합니다.
  */
 @Service
 public class UserService {
@@ -20,6 +20,7 @@ public class UserService {
 
     /**
      * UserService 생성자.
+     *
      * @param userRepository 사용자 데이터베이스 접근 객체
      * @param passwordEncoder 비밀번호 암호화를 위한 BCryptPasswordEncoder
      */
@@ -46,12 +47,38 @@ public class UserService {
     }
 
     /**
-     * 사용자 이름으로 사용자 조회.
+     * 아이디의 사용 가능 여부를 확인합니다.
+     *
+     * @param username 확인할 사용자 이름
+     * @return 사용 가능하면 true, 사용 중이면 false
+     */
+    public boolean checkUsernameAvailability(String username) {
+        return !userRepository.existsByUsername(username);
+    }
+
+    /**
+     * 사용자 이름으로 사용자 정보를 조회합니다.
+     *
      * @param username 찾을 사용자 이름
      * @return 사용자 객체를 Optional로 반환 (존재하지 않으면 빈 Optional 반환)
      */
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    /**
+     * 사용자 등록.
+     * 아이디 중복 여부를 확인하고, 비밀번호를 암호화하여 저장합니다.
+     *
+     * @param user 등록할 사용자 객체
+     * @throws UsernameAlreadyExistsException 아이디가 이미 존재할 경우 예외 발생
+     */
+    public void register(User user) throws UsernameAlreadyExistsException {
+        if (isUsernameTaken(user.getUsername())) {
+            throw new UsernameAlreadyExistsException("이미 사용 중인 아이디입니다: " + user.getUsername());
+        }
+        user.setPassword(encryptPassword(user.getPassword()));
+        saveUser(user);
     }
 
     /**
@@ -72,28 +99,5 @@ public class UserService {
     private void saveUser(User user) {
         userRepository.save(user);
     }
-
-    /**
-     * 사용자 등록.
-     * 아이디 중복 여부를 확인하고, 비밀번호를 암호화하여 저장합니다.
-     *
-     * @param user 등록할 사용자 객체
-     * @throws UsernameAlreadyExistsException 아이디가 이미 존재할 경우 예외 발생
-     */
-    public void register(User user) throws UsernameAlreadyExistsException {
-        if (isUsernameTaken(user.getUsername())) {
-            throw new UsernameAlreadyExistsException("이미 사용 중인 아이디입니다: " + user.getUsername());
-        }
-        user.setPassword(encryptPassword(user.getPassword()));
-        saveUser(user);
-    }
-
-    /**
-     * 아이디의 사용 가능 여부 확인.
-     * @param username 확인할 사용자 이름
-     * @return 사용 가능하면 true, 사용 중이면 false
-     */
-    public boolean checkUsernameAvailability(String username) {
-        return !userRepository.existsByUsername(username);
-    }
 }
+
