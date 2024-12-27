369e3a9702f39c5348eaec2fccefaa91db00c79e
qncj5224 <qncj5224@naver.com>
2024-12-27 22:21:25 +0900

    .

--- src/main/java/com/example/tourist_recommendation/service/UsernameAlreadyExistsException.java
+++ src/main/java/com/example/tourist_recommendation/service/UsernameAlreadyExistsException.java
@@ -1,4 +1,10 @@
 package com.example.tourist_recommendation.service;
-
-public class UsernameAlreadyExistsException {
+/**
+ * 아이디 중복 예외 클래스.
+ * 아이디가 이미 사용 중일 때 발생합니다.
+ */
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
 }
